8db2f2032a2f6525a1c866ab43f0e3088c4c8a19
Parasdheeraj <angulardevloper@parastechnologies.com>
2025-06-13 15:49:26 +0530

    remove stripe key

--- apps/pet-shop-web/src/app/app.component.ts
+++ apps/pet-shop-web/src/app/app.component.ts
@@ -11,6 +11,5 @@ import { HeaderComponent } from './components/header/header.component';
 })
 export class AppComponent {
   title = 'pet-shop-web';
-  //pk_test_51RZUI3P0qFDfcI830WR0VtZgY5Jny9PizqEf6xSiDcIkbB5Mad21r9s0Vwf4f7uu93MoCjtj3mIU1cj6nGQxvyYO00ARBAn7K9
-  //sk_test_51RZUI3P0qFDfcI83GYgwF594i2Jd67bGFO2ZQsl37F4cUp7UlXsfINXcHEUPADHAMyevYJVORNVTgB6pQ8brRQUk00t1SxKqud
+  
 }
